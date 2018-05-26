00e1c77bffd04e730729df46ac835bbca8eb36c8
Nguyễn Ngọc Quang <ngocquang994@gmail.com>
2018-05-26 19:04:11 +0700

    update

--- node2.js
+++ node2.js
@@ -8,6 +8,7 @@ var io = require('socket.io').listen(app);
 //Simple echo server
 io.on('connection', function(socketconnection){
     socketconnection.send("Connected to Server-2");
+    console.log('Connected to Server-2');
      
     socketconnection.on('message', function(message){
         socketconnection.send(message);
